317d99ac11ab08de619e1221e6ec56bc88975437
priyanga <priyangameha14@gmail.com>
2023-08-11 13:07:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Header from './common/header/Header';
+import{BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import Products from './common/pages/Products';
+import Data from "./Components/Data"
+import Cart from "./common/Cart/Cart"
+import Sdata from "./Components/shop/Sdata"
+import Shop from "./Components/shop/Shop"
+import { useState } from 'react';
+import Footer from "./common/Footer/Footer"
+const App = () => {
+  const { productItems } = Data
 
-function App() {
+  const { shopItems } = Sdata
+
+  const [CartItem, setCartItem] = useState([])
+
+  const addToCart = (product) => {
+
+    const productExit = CartItem.find((item) => item.id === product.id)
+    if (productExit) {
+
+      setCartItem(CartItem.map((item) => (item.id === product.id ? { ...productExit, qty: productExit.qty + 1 } : item)))
+    } 
+    else {
+      setCartItem([...CartItem, { ...product, qty: 1 }])
+    }
+  }
+  const decreaseQty = (product) => {
+    
+    const productExit = CartItem.find((item) => item.id === product.id)
+
+    
+    if (productExit.qty === 1) {
+      setCartItem(CartItem.filter((item) => item.id !== product.id))
+    } else {
+     
+      setCartItem(CartItem.map((item) => (item.id === product.id ? { ...productExit, qty: productExit.qty - 1 } : item)))
+    }
+    
+  }
+  const deleteProduct =(product)=>
+    {
+      setCartItem(CartItem.filter((item) => item.id !== product.id))
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+      
+      <Router>
+      <Header CartItem={CartItem}/>
+      <Switch>
+       <Route exact path='/' >
+            <Products productItems={productItems}  addToCart={addToCart} shopItems={shopItems} />
+          </Route>
+          <Route path='/product' exact>
+          <Shop  shopItems={shopItems} addToCart={addToCart}/>
+          </Route>
+          <Route path='/cart' exact>
+            <Cart CartItem={CartItem} addToCart={addToCart} decreaseQty={decreaseQty} deleteProduct={deleteProduct}/>
+          </Route>
+          </Switch>
+          <Footer />
+    </Router>
+    
+    </>
+    
+  )
 }
 
-export default App;
+export default App
